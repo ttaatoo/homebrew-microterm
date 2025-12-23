d345602b4ec5c543919e31f3a96997be08d143d6
taoyi <yitao@moonton.com>
2025-12-23 23:12:08 +0800

    fix: use ASCII filename for DMG URL

--- Casks/microterm.rb
+++ Casks/microterm.rb
@@ -2,7 +2,7 @@ cask "microterm" do
   version "1.1.0"
   sha256 "6e3b14fc8602ae25f3b2729dce8ef6eee8a8e622b028ace06230475dddf655b7"
 
-  url "https://github.com/ttaatoo/microterm/releases/download/v#{version}/µTerm_#{version}_aarch64.dmg"
+  url "https://github.com/ttaatoo/microterm/releases/download/v#{version}/microterm_#{version}_aarch64.dmg"
   name "µTerm"
   desc "Lightweight macOS menubar terminal application"
   homepage "https://github.com/ttaatoo/microterm"
